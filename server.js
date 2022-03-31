a0317cb627cf653b71013f7f085633b83a8d5796
Senna Annaba Ahmad <sennaannaba@gmail.com>
2022-03-31 18:39:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get('/api/:date?', function (req, res) {
+app.get('/api/:date', function (req, res) {
     var date
     if (!req.params.date) {
         date = new Date();
